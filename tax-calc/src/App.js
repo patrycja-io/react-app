bc8a3817360d81b98f9e51d1efc03a9f25353194
patrycja-io <patrycjatyra@protonmail.com>
2020-11-26 23:50:39 +0000

    redesigned app

--- tax-calc/src/App.js
+++ tax-calc/src/App.js
@@ -1,8 +1,22 @@
+import React from 'react';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeTax';
+import { NewCalculation } from './components/Calculation';
+import { GlobalProvider } from './context/GlobalState';
+
 import './App.css';
 
 function App() {
   return (
-   
+    <GlobalProvider>
+      <Header />
+      <div className="container"> 
+      <NewCalculation />
+      <IncomeExpenses />
+        <Balance />
+      </div>
+    </GlobalProvider>
   );
 }
 
