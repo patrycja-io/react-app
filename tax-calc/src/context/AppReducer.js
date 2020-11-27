9c61373f770d437d64f39dc36e28dc76a6280c84
patrycja-io <patrycjatyra@protonmail.com>
2020-11-27 00:14:14 +0000

    added reducer

---
+++ tax-calc/src/context/AppReducer.js
@@ -0,0 +1,11 @@
+export default (state, action) => {
+    switch(action.type) {
+      case 'ADD_INCOME':
+        return {
+          ...state,
+          annualIncome: [action.payload, ...state.annualIncome]
+        }
+      default:
+        return state;
+    }
+  }
